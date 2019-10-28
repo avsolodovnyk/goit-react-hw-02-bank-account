6d5f919c664b80bd0fec13872e5f7c2a7dcea3a0
Anton S <av.solodovnyk@gmail.com>
2019-10-28 23:59:28 +0200

    Updates

---
+++ precache-manifest.e6146e5b9d52f2fc61b05b57303a2a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bf3d55b07352fe11667d6e686db4c2",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "2fb05b70afe6389f3fa7",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "856c317816a62bc5842d",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.66d6dc3f.chunk.css"
+  },
+  {
+    "revision": "2fb05b70afe6389f3fa7",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.76abd22f.chunk.js"
+  },
+  {
+    "revision": "856c317816a62bc5842d",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.c2b0c507.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
